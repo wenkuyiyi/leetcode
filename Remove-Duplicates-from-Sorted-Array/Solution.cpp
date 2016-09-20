2b52b4dc8efb3551f03ac256814830409d1ff78c
wenkuyiyi <1307394263@qq.com>
2016-09-20 12:11:56 +0800

    Update Solution.cpp

--- Remove-Duplicates-from-Sorted-Array/Solution.cpp
+++ Remove-Duplicates-from-Sorted-Array/Solution.cpp
@@ -8,17 +8,16 @@ using namespace std;
 class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
-	    if(nums.size() == 0)    return 0;   //必须考虑长度为0的情况
+	if(nums.size() == 0)    return 0;   //必须考虑长度为0的情况
 	    
-		int news = 0;
-		unsigned int sized = nums.size();   // vs上<vector>size()返回值必须为unsighed int；
-		for (unsigned int j = 1; j < nums.size(); j++)   // i,j的遍历
-			if (nums[j] != nums[j - 1])
-			{
-				nums[news+1] = nums[j];
-				news++;
-				
-			}
+	int news = 0;
+	unsigned int sized = nums.size();   // vs上<vector>size()返回值必须为unsighed int；
+	for (unsigned int j = 1; j < nums.size(); j++)   // i,j的遍历
+		if (nums[j] != nums[j - 1])
+		{
+			nums[news+1] = nums[j];
+			news++;
+		}
 		for (int i = 0; i < sized - news - 1; i++)   
 			nums.pop_back();
 		return news + 1;
